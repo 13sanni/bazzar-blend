4e8c29769b8b0adb0d893ea88a7f303cbf6cca92
ss752 <ss7527767@gmail.com>
2025-11-25 19:39:15 +0530

    small change

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send({message:'Welcome To Zosh Bazaar Backend System!'});
+  res.send({message:'Welcome To blend bazzar Backend System!'});
 });
 
 app.use(bodyParser.json());
