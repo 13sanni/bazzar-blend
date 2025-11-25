86c0f847d09c0c3b1dc626cc5ec35c11e6fc32e2
ss752 <ss7527767@gmail.com>
2025-11-25 21:20:53 +0530

    SMALL CHANGE

--- frontend/src/Config/Api.js
+++ frontend/src/Config/Api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 export const API_URL = "http://localhost:8080";
-export const DEPLOYED_URL = "https://bazzar-blend.vercel.app/";
+export const DEPLOYED_URL = "https://bazzar-blend-backend.vercel.app";
 // change api
 export const api = axios.create({
-    baseURL: API_URL,
+    baseURL:DEPLOYED_URL || API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
